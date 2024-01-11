3c96a5a44d3b078d1dedb7463e38b346245dd2b2
jml74262 <jml74262@lasallebajio.edu.mx>
2024-01-10 18:03:28 -0600

    Add project files.

---
+++ reactCRUD.Server/Controllers/UsuarioController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using reactCRUD.Server.Models;
+
+namespace reactCRUD.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly testReactContext _testReactContext;
+
+        public UsuarioController(testReactContext context) 
+        {
+            _testReactContext = context;    
+        }
+
+        [HttpGet]
+        [Route("Lista")]
+        public async Task<IActionResult> Lista()
+        {
+            List<Usuario> lista = await _testReactContext.Usuarios.OrderByDescending(u => u.IdUser).ToListAsync();
+
+            return StatusCode(StatusCodes.Status200OK, lista);
+        }
+
+        [HttpPost]
+        [Route("Guardar")]
+        public async Task<IActionResult> Guardar([FromBody] Usuario request)
+        {
+            await _testReactContext.Usuarios.AddAsync(request);
+            await _testReactContext.SaveChangesAsync();
+
+            return StatusCode(StatusCodes.Status200OK, "ok");
+        }
+
+        [HttpPut]
+        [Route("Editar")]
+        public async Task<IActionResult> Editar([FromBody] Usuario request)
+        {
+            _testReactContext.Usuarios.Update(request);
+            await _testReactContext.SaveChangesAsync();
+
+            return StatusCode(StatusCodes.Status200OK, "ok");
+        }
+
+        [HttpDelete]
+        [Route("Eliminar/{id:int}")]
+        public async Task<IActionResult> Eliminar(int id)
+        {
+            Usuario usuario = _testReactContext.Usuarios.Find(id);
+            _testReactContext.Usuarios.Remove(usuario);
+            await _testReactContext.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status200OK, "ok");
+        }
+    }
+}
---
+++ reactCRUD.Server/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace reactCRUD.Server.Models
+{
+    public partial class Usuario
+    {
+        public int IdUser { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public string? Telefono { get; set; }
+        public string? Correo { get; set; }
+        public string? Contrasena { get; set; }
+    }
+}
